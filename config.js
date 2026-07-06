55eeb480eceee3e3153a8f878e6a62da56276198
renitaazzahrasalsabilla-ship-it <renitaazzahrasalsabilla@gmail.com>
2026-07-06 22:14:47 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+// Ganti URL di bawah ini dengan endpoint MockAPI project kamu sendiri.
+// Contoh: https://64abc123.mockapi.io/api/users
+export const API_URL = 'https://6a4bab5ff5eab0bb6b633589.mockapi.io/users'
